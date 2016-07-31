29d0047082a0a683c52f81f91afc40abf62a6532
Vivien Richter <chri_ri@hotmail.de>
2016-07-31 20:08:02 +0200

    Add result panel and buttons

--- locale/en.inc.php
+++ locale/en.inc.php
@@ -5,5 +5,7 @@
  
     define('LC_LANG', 'en');
 	define('LC_TITLE', 'Cut manager');
-	define('LC_HEADLINE', 'Calculate');
+	define('LC_HEADLINE', 'Calculation');
+	define('LC_BTN_CALC', 'Calculate');
+	define('LC_BTN_SAVE', 'Save');
 ?>
